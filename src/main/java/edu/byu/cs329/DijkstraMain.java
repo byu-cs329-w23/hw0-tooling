e5ab10ab3f390479f28ee01000df7a5b0b542c71
haltosan <ethanhsimmons@gmail.com>
2023-01-12 11:25:57 -0700

    Add logging

--- src/main/java/edu/byu/cs329/DijkstraMain.java
+++ src/main/java/edu/byu/cs329/DijkstraMain.java
@@ -1,10 +1,14 @@
 package edu.byu.cs329;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Runner for Dijkstra.
  *
  */
 public class DijkstraMain {
+
 
   /**
    * Supply a graph to the Dijkstra solver.
@@ -12,13 +16,14 @@ public class DijkstraMain {
    * @param args commandline arguments (for compatibility, not used)
    */
   public static void main(String[] args) {
+    final Logger logger = LoggerFactory.getLogger(Dijkstra.class);
     final int[][] graph = { { 0, 2, 4, Dijkstra.M, Dijkstra.M, Dijkstra.M },
         { 2, 0, 1, 4, Dijkstra.M, Dijkstra.M }, { 4, 1, 0, 2, 6, Dijkstra.M },
         { Dijkstra.M, 4, 2, 0, 1, 3 }, { Dijkstra.M, Dijkstra.M, 6, 1, 0, 5 },
         { Dijkstra.M, Dijkstra.M, Dijkstra.M, 3, 5, 0 } };
     Dijkstra d = new Dijkstra(graph);
     int sp = d.shortestPath(0, 4);
-    System.out.println("d = " + sp);
+    logger.info("d = " + sp);
   }
 
 }
